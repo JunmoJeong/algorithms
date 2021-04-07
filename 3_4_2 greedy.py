7e350e67c3be9cc46fa7e76e0c168a5bd0d0d150
junmojeong <jjmstars7@gmail.com>
2021-04-07 13:42:51 +0900

    3_4_2 solution

--- 3_4_2 greedy.py
+++ 3_4_2 greedy.py
@@ -14,24 +14,23 @@ N과 K가 주어질 때 N이 1이 될때까지 1번 혹은 2번의 과정을 수
 2 이상의 수로 나누는 것이 1을 빼는 것보다 숫자를 훨씬 많이 줄일 수 있다. 
 '''
 
-
+# n, k 공백으로 구분하여 입력받기
 n, k = map(int, input().split())
+result = 0 
 
-result = 0
+while True:
+    # n == k로 나누어 떨어지는 수가 될 때까지 1씩 빼기
+    target = (n//k) * k
+    result += (n - target)
+    n = target
+    # n이 k보다 작을 때 반복문 탈출
+    if n < k:
+        break
 
-# N이 K 이상이라면 K로 계속 나누기
-while n >= k:
-    # n이 k로 나누어 떨어지지 않는다면 n에서 1씩 빼기
-    while n % k != 0:
-        n -= 1
-        result += 1
     # k로 나누기
-    n //= k
     result += 1
+    n //= k
 
 # 마지막으로 남은 수에 대하여 1씩 빼기
-while n > 1:
-    n -= 1
-    result += 1
-
+result += (n-1)
 print(result)
