8573a09b60ca169ef65359507da0d23515d72561
kakao <jjmstars7@gmail.com>
2021-08-03 03:34:24 +0900

    stack

--- data_structure_practice.py
+++ data_structure_practice.py
@@ -34,3 +34,15 @@ data_stack.append(1)
 data_stack.append(2)
 
 data_stack.pop()
+
+stack_list = list()
+
+
+def push(data):
+    stack_list.append(data):
+
+
+def pop():
+    data = stack_list[-1]
+    del stack_list[-1]
+    return data
