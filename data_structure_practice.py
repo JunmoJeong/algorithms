1bfd8edfd59213d565b9784b898aad3f1aadd407
kakao <jjmstars7@gmail.com>
2021-08-08 19:44:11 +0900

    linked list

--- data_structure_practice.py
+++ data_structure_practice.py
@@ -68,3 +68,30 @@ class Node:
 
 node1 = Node(1)
 node2 = Node(2)
+node1.next = node2
+head = node1
+
+# data 추가
+
+
+class Node:
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+
+
+def add(data):
+    node = head
+    while node.next:  # next가 있다면
+        node = node.next
+
+    node.next = Node(data)
+
+
+node1 = Node(1)
+head = node1
+for index in range(2, 10):
+    add(index)
+
+for i in range(2, 10):
+    print(i)
