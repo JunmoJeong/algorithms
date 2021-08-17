a05c4c0f242d4fbd5fab58d184a50d86409ee85b
kakao <jjmstars7@gmail.com>
2021-08-18 06:49:09 +0900

    linked list

--- data_structure_practice.py
+++ data_structure_practice.py
@@ -152,3 +152,29 @@ linkedlist.desc()
 for data in range(1, 10):
     linkedlist1.add(data)
 linkedlist1.desc()
+
+
+class Node:
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+
+
+class NodeMgmt:
+    def __self__(self, data):
+        self.head = Node(data)
+
+    def add(self, data):
+        if self.head == "":
+            self.head = Node(data)
+        else:
+            node = self.head
+            while node.next:
+                node = node.next
+            node.next = Node(data)
+
+    def desc(self):
+        node = self.head
+        while node:
+            print(node.data)
+            node = node.next
