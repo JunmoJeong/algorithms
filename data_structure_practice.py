2ed937c4d373a753085f2adabf958c84e7e6acee
kakao <jjmstars7@gmail.com>
2021-08-21 06:16:18 +0900

    double linked list ex.

--- data_structure_practice.py
+++ data_structure_practice.py
@@ -275,3 +275,35 @@ class NodeMgmt:
         self.tail = self.head
 
     def insert(self, data):
+
+
+class Node:
+    def __init__(self, data, prev=None, next=None):
+        self.prev = prev
+        self.data = data
+        self.next = next
+
+
+class NodeMgmt:
+    def __init__(self, data):
+        self.head = Node(data)
+        self.tail = self.head
+
+    def insert(self, data):
+        if self.head = None:
+            self.head = Node(data)
+            self.tail = self.head
+        else:
+            node = self.head
+            while node.next:
+                node = node.next
+            new = Node(data)
+            node.next = new
+            new.prev = node
+            self.tail = new
+
+        def desc(self):
+            node = self.head
+            while node:
+                print(node.data)
+                node = node.next
