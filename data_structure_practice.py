c4a4d46c2b38018c8004e3226688c1b406e661cf
kakao <jjmstars7@gmail.com>
2021-08-21 04:40:22 +0900

    lined list ex.

--- data_structure_practice.py
+++ data_structure_practice.py
@@ -209,4 +209,18 @@ class NodeMgmt:
     def __init__(self, data):
         if self.head == "":
             self.head = Node(data)
+
+    def add(self, data):
+        if self.head == '':
+            self.head = Node(data)
         else:
+            node = self.head
+            while node.next:
+                node = node.next
+            node.next = Node(data)
+
+    def desc(self):
+        node = self.head
+        while node:
+            print(node.data)
+            node = node.next
