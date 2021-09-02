a7fed4d2dcfde8b298204ccb83a3e33f212e8e55
JunmoJeong <jjmstars7@gmail.com>
2021-09-02 21:44:34 +0900

    another mac git test

--- 12969.py
+++ 12969.py
@@ -4,16 +4,17 @@
 '''
 
 # 풀이 : 반복문을 돌리기 위해 n,m을 int형으로 바꾼다. 우선 두 개의 입력이 공백을 사이에 두고 받으므로
-# split을 이용해 분리하여 받는다. 
+# split을 이용해 분리하여 받는다.
 
-# input으로 받는 값들은 string 자료형을 가지고 있으므로 int형으로 바꿔야 한다. 
+# input으로 받는 값들은 string 자료형을 가지고 있으므로 int형으로 바꿔야 한다.
 
 a, b = map(int, input().split(' '))
-answer =''
+answer = ''
 for i in range(b):
     for j in range(a):
-        answer +="*"
+        answer += "*"
     answer += '\n'
 
-#print(answer)
+# print(answer)
+print(answer)
 print(answer)
