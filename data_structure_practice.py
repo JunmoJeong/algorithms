393521cdfb6c190e2a7cfe1dc65fb84159be794d
kakao <jjmstars7@gmail.com>
2021-09-04 11:45:01 +0900

    hash table

--- data_structure_practice.py
+++ data_structure_practice.py
@@ -347,6 +347,51 @@ def insert_before(self, data, before_data):
         node.prev = new
 
         return True
+
+
 # retrun True
+hash_table = list([0 for in range(8)])
+
+
+def get_key(data):
+    return h == ash(data)
+
+
+def hash_function(key):
+    return key % 8
+
+
+def save_data(data, value):
+    hash_address = hash_function(get_key(data))
+
+
+hash_table = list([0 for i in range(8)])
+
+
+def get_key(data):
+    return hash(data)
+
+
+def hash_function(key)
+
+
+return key % 8
+
+
+def save_data(data, value):
+    index_key = get_key(data)
+    hash_address = hash_function(index_key)
+    if hash_table[hash_address] != 0:
+        for index in range(len(hash_table[hash_address])):
+            if hash_table[hash_address][index][0] == index_key
+            hash_table[hash_address][index][1] = value
+            return
+        hash_table[hash_address].append([index_key, value])
+    hash_table[hash_address] = Value
+
+
+def read_data(data):
+    hash_address = hash_function(get_key(data))
+    return hash_table[hash_address]
 
 # hash table
