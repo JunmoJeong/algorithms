788f758be1f5562403ea48f0d3a37bd586cbacef
kakao <jjmstars7@gmail.com>
2021-11-12 08:38:06 +0900

    practice

--- test.py
+++ test.py
@@ -1,18 +1,18 @@
-def func_a(times):
-    hour = int(times[:2])
-    minute = int(times[3:])
-    return hour*60 + minute
+def solution(arr):
+    left, right = 0, len(arr) - 1
+    idx = 0
+    answer = [0 for _ in range(len(arr))]
+    while left <= right:
+        if left % == 0:
+            answer[idx] = arr[left]
+            left += 1
+        else:
+            answer[idx] = arr[right]
+            right -= 1
+        idx += 1
+    return answer
 
+    arr = [1, 2, 3, 4, 5, 6]
+    ret = solution(arr)
 
-def solution(subway_times, current_time):
-    current_minute = func_a(current_time)
-    INF = 10000000
-    answer = INF
-    for s in subway_times:
-        subway_minute = func_a(s)
-        if subway_minute >= current_minute:
-            answer = subway_minute - current_minute
-            break
-        if answer == INF:
-            return -1
-        return answer
+    print("solution 함수의 반환 값은 ", ret, "입니다.")
