d7c567611e494e36d9d13ae3ff885eb5fad946f1
JunmoJeong <jjmstars7@gmail.com>
2021-11-18 16:14:56 +0900

    python api

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
+from flask import Flask
+
+
 def add(a, b):
     return a+b
 
@@ -7,3 +10,12 @@ def same(a, b):
         return True
     else:
         return False
+
+
+# app.py
+app = Flask(__name__)
+
+
+@app.route('/')
+def greeting():
+    return "This is Test API!"
