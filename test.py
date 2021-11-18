787f0280f05d5fe4516f0e959e213a97e25c2f56
JunmoJeong <jjmstars7@gmail.com>
2021-11-18 16:34:54 +0900

    api

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+from Study import *
+import json
 from flask_api import status
 from flask import Flask, request
 from flask import Flask
@@ -21,3 +23,21 @@ app = Flask(__name__)
 @app.route('/')
 def greeting():
     return "This is Test API!"
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def greeting():
+    return "This is Test API!"
+
+
+@app.route('/add', methods=['POST'])
+def get_add():
+    if request.method == 'POST':
+        a = request.json['a']
+        b = request.json['b']
+        c = add(a, b)
+        result = json.dumps(c)
+    return result, status.HTTP_200_OK, {}
