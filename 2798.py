b637af944f9d352e1f5e76c1cf02a17a2f594d9d
JunmoJeong <jjmstars7@gmail.com>
2021-12-07 00:49:43 +0900

    algo

--- 2798.py
+++ 2798.py
@@ -1,2 +1,15 @@
 n, m = list(map(int, input().split(' ')))
 data = list(map(int, input().split(' ')))
+
+
+result = 0
+length = len(data)
+
+for i in range(0, length):
+    for j in range(i+1, length):
+        for k in range(j+1, length):
+            sum_value = data[i] + data[j] + data[k]
+            if sum_value <= m:
+                result = sum_value
+
+print(result)
