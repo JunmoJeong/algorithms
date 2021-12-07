fd0bdb1b1771e4dd507b6174c63c53ff3afbb22e
JunmoJeong <jjmstars7@gmail.com>
2021-12-07 22:18:54 +0900

    time

--- time.py
+++ time.py
@@ -1,1 +1,12 @@
-print("hi")
+from datetime import datetime
+
+now = datetime.now()
+current_hour = now.strftime("%H")
+current_hour = int(current_hour)
+
+if current_hour % 2 == 0:
+    with open("time.txt", "a") as f:
+        current_hour = str(current_hour)
+        f.write(current_hour + '시\n')
+else:
+    print("홀수 시간입니다.")
