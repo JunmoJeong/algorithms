d0684a43dbe4e50c015b6dfe1e9ec4a7828e2565
JunmoJeong <jjmstars7@gmail.com>
2021-12-10 07:41:28 +0900

    algo

--- 계산기.py
+++ 계산기.py
@@ -7,3 +7,21 @@ while True:
     os.system("pause")
 
 '''
+
+import os
+operator = ["+", "-", "*", "/", "="]
+
+
+def string_calculator(user_input, show_history=False):
+    string_list = []
+    lop = 0
+
+    if user_input[-1] not in operator:
+        user_input = "="
+
+    for i, s in enumerate(user_input):
+        if s in operator:
+            if user_input[lop:i].strip() != "":
+                string_list.append(user_input[lop:i])
+                string_list.append(s)
+                lop = i + 1
