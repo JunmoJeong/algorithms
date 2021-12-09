785b49b1b6eeb2b44cf0b5b6bca3e71e97f5a6d5
JunmoJeong <jjmstars7@gmail.com>
2021-12-09 20:41:14 +0900

    algo

---
+++ 1874.py
@@ -0,0 +1,5 @@
+n = int(input())
+
+count = 1
+stack = []
+result = []
--- 계산기.py
+++ 계산기.py
@@ -1,1 +1,9 @@
-import os
+''' import os
+
+while True:
+    os.system("cls")
+    s = input("계산식 입력>")
+    print("결과: {}".format(eval(s)))
+    os.system("pause")
+
+'''
