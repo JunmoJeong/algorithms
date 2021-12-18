5102e4e7c90cfc0c1ba9b65df82fe6d1f4639c6b
JunmoJeong <jjmstars7@gmail.com>
2021-12-18 16:03:34 +0900

    python practice

--- 계산기.py
+++ 계산기.py
@@ -1,27 +1,6 @@
-''' import os
-
-while True:
-    os.system("cls")
-    s = input("계산식 입력>")
-    print("결과: {}".format(eval(s)))
-    os.system("pause")
-
-'''
-
 import os
+
 operator = ["+", "-", "*", "/", "="]
 
 
 def string_calculator(user_input, show_history=False):
-    string_list = []
-    lop = 0
-
-    if user_input[-1] not in operator:
-        user_input = "="
-
-    for i, s in enumerate(user_input):
-        if s in operator:
-            if user_input[lop:i].strip() != "":
-                string_list.append(user_input[lop:i])
-                string_list.append(s)
-                lop = i + 1
