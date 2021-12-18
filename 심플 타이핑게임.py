a3d4f49f74dd94feb0340a0354676718b3780653
JunmoJeong <jjmstars7@gmail.com>
2021-12-18 16:37:41 +0900

    practice

--- 심플 타이핑게임.py
+++ 심플 타이핑게임.py
@@ -1,11 +1,8 @@
 # 타이핑 게임 만들기
 
-import time
-import random
-
 import os
-
-
+import random
+import time
 WORD_LIST = [
     "남박사의 파이썬 100% 실전 프로그래밍 강좌",
     "파이썬에서 ord() 함수는 문자의 유니코드 값을 알아오는 함수로 10진수 값을 리턴합니다.",
@@ -16,12 +13,14 @@ WORD_LIST = [
     "코딩하세요 코딩~~",
     "독도는 우리땅"
 ]
+
+
 random.shuffle(WORD_LIST)
 
 for q in WORD_LIST:
     os.system("cls")
     start_time = time.time()
-    user_input = str(input(q + '\n')).strip()
+    user_input = str(input(q+'\n')).strip()
     end_time = time.time() - start_time
 
     if user_input == "/exit":
@@ -33,10 +32,11 @@ for q in WORD_LIST:
             break
         if c == q[i]:
             correct += 1
-    tot_len = len(q)
-    c = correct / tot_len * 100
-    e = (tot_len - correct) / tot_len * 100
-    speed = (correct / end_time) * 60
 
-    print("속도: {:0.2f} 정확도: {:0.2f} 오타율: {:0.2f}".format(speend, c, e))
-    os.system("pause")
+tot_len = len(q)
+c = correct / tot_len * 100
+e = (tot_len - correct) / tot_len * 100
+speed = (correct / end_time) * 60
+
+print("속도: {} 정확도: {} 오타율: {}".format(speed, c, e))
+os.system("pause")
