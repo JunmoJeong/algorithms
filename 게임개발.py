41710c65b448a537de9a62ebe4c3d8cc0d8c7d76
JunmoJeong <jjmstars7@gmail.com>
2022-01-03 20:28:56 +0900

    test

--- 게임개발.py
+++ 게임개발.py
@@ -3,30 +3,34 @@
 
 
 # n,m을 공백으로 구분하여 입력받기
-n,m = map(int,input().split(' '))
+n, m = map(int, input().split(' '))
 
 # 방문한 위치를 저장하기 위한 맵을 생성하여 0으로 초기화
 d = [[0]*m for _ in range(n)]
 
 # 현재 캐릭터의 x좌표, y좌표, 방향을 입력받기
-x, y, direction = map(int,input().split(' '))
-d[x][y] = 1 # 현재 좌표 방문 처리
+x, y, direction = map(int, input().split(' '))
+d[x][y] = 1  # 현재 좌표 방문 처리
 
 # 전체 맵 정보를 입력받ㄷ기
 array = []
 for i in range(n):
-    array.append(list(map(int,input().split(' '))))
+    array.append(list(map(int, input().split(' '))))
 
 # 북, 동, 남, 서 방향 정의
-dx = [-1,0,1,0]
-dy = [0,1,0,-1]
+dx = [-1, 0, 1, 0]
+
+dy = [0, 1, 0, -1]
 
 # 왼쪽으로 회전
+
+
 def turn_left():
     global direction
     direction -= 1
     if direction == -1:
         direction = 3
+
 
 # 시뮬레이션 시작
 count = 1
@@ -38,8 +42,8 @@ while True:
     ny = y + dy[direction]
 
     # 회전한 이후 정면에 가보지 않은 칸이 존재하는 경우 이동
-    if d[nx][ny] == 0 and array[nx][ny] ==0:
-        d[nx][ny] =1
+    if d[nx][ny] == 0 and array[nx][ny] == 0:
+        d[nx][ny] = 1
         x = nx
         y = ny
         count += 1
@@ -49,7 +53,6 @@ while True:
     else:
         turn_time += 1
 
-
     # 네 방향 모두 갈 수 없는 경우
     if turn_time == 4:
         nx = x - dx[direction]
@@ -57,7 +60,7 @@ while True:
 
         # 뒤로 갈 수 있다면 이동하기
         if array[nx][ny] == 0:
-            x = nx 
+            x = nx
             y = ny
 
         # 뒤가 바다로 막혀있는 경우
