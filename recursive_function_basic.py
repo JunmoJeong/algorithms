4de3b051c00ccd1c0cea260bf8cf91af4922642d
JunmoJeong <jjmstars7@gmail.com>
2022-01-04 07:47:53 +0900

    dfs sol

--- recursive_function_basic.py
+++ recursive_function_basic.py
@@ -1,6 +1,23 @@
+'''
 def recursive_function():
     print('재귀 함수를 호출합니다.')
     recursive_function()
 
 
 recursive_function()
+
+
+'''
+
+
+def recursive_function(i):
+    # 100번 째 출력했을 때 종료되도록 종료 조건 명시
+    if i == 100:
+        return
+
+    print(i, '번째 재귀 함수에서', i+1, '번째 재귀 함수를 호출합니다.')
+    recursive_function(i+1)
+    print(i, '번째 재귀 함수를 종료합니다.')
+
+
+recursive_function(1)
