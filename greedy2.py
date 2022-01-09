f0fc35ae5a5d8dd9c3693c44a137fd570f0ff336
JunmoJeong <jjmstars7@gmail.com>
2022-01-10 06:54:07 +0900

    test

--- greedy2.py
+++ greedy2.py
@@ -2,16 +2,17 @@
 n, m = map(int, input().split(' '))
 
 # 방문한 위치를 저장하기 위한 맵을 생성하여 0으로 초기화
-d = [[0] * m for _ in range(n)]
+d = [[0]*m for _ in range(n)]
 
-# 현재 캐릭터의 X좌표, Y좌표, 방향을 입력받기
+# 현재 캐릭터의 x좌표, y좌표, 방향을 입력받기
 x, y, direction = map(int, input().split(' '))
 d[x][y] = 1  # 현재 좌표 방문 처리
 
-# 전체 맵 정보를 입력 받기
+# 전체 맵 정보를 입력받기
+
 array = []
 for i in range(n):
-    array.appennd(list(map(int, input().split(' '))))
+    array.append(list(map(int, input().split(' '))))
 
 # 북, 동, 남, 서 방향 정의
 dx = [-1, 0, 1, 0]
@@ -30,9 +31,8 @@ def turn_left():
 # 시뮬레이션 시작
 count = 1
 turn_time = 0
-
 while True:
-    # 왼쪽으로 회전
+    # 외쪽으로 회전
     turn_left()
     nx = x + dx[direction]
     ny = y + dy[direction]
@@ -44,13 +44,14 @@ while True:
         y = ny
         count += 1
         turn_time = 0
+
         continue
 
-    # 회전한 이후 정면에 가보지 않은 칸이 없거나 바다인 경우
+    # 회전한 이후 정면에 ㄱ보지 않은 칸이 없거나 바다인 경우
     else:
         turn_time += 1
 
-    # 네 방향 모두ㅜ 갈 수 없는 경우
+    # 네 방향 모두 갈 수 없는 경우
     if turn_time == 4:
         nx = x - dx[direction]
         ny = y - dy[direction]
@@ -65,4 +66,4 @@ while True:
             break
         turn_time = 0
 
-print(count)
+printn(count)
