67e8503d785f02618a473524e6846f5e320d07fb
JunmoJeong <jjmstars7@gmail.com>
2022-01-12 06:47:46 +0900

    again solve

--- 왕실의나이트.py
+++ 왕실의나이트.py
@@ -6,19 +6,18 @@ column = int(ord(input_data[0])) - int(ord('a')) + 1
 steps = [
     (-2, -1), (-1, -2), (1, -2), (2, -1),
     (2, 1), (1, 2), (-1, 2), (-2, 1)
-
 ]
-
 # 8가지 방향에 대하여 각 위치로 이동이 가능한지 확인
 result = 0
 
 for step in steps:
     # 이동하고자 하는 위치 확인
-    next_row = row + step[0]
+    next_row = row+step[0]
     next_column = column + step[1]
 
     # 해당 위치로 이동이 가능하다면 카운트 증가
-    if next_row >= 1 and next_row <= 8 and next_column >= 1 and next_column <= 8:
+    if next_row >= 1 and next_row <= 8 and next_column >= 1 \
+            and next_column <= 8:
         result += 1
 
 print(result)
