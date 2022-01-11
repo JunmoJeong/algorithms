541f9ffb83223e38bd110a052486bb3d0259a516
JunmoJeong <jjmstars7@gmail.com>
2022-01-12 06:54:17 +0900

    again solve

--- 게임개발.py
+++ 게임개발.py
@@ -1,8 +1,4 @@
-# 0:북 1:동 2:남 3:서
-# 0:육 1:바
-
-
-# n,m을 공백으로 구분하여 입력받기
+# N, M 을 공백으로 구분하여 입력받기
 n, m = map(int, input().split(' '))
 
 # 방문한 위치를 저장하기 위한 맵을 생성하여 0으로 초기화
@@ -12,14 +8,14 @@ d = [[0]*m for _ in range(n)]
 x, y, direction = map(int, input().split(' '))
 d[x][y] = 1  # 현재 좌표 방문 처리
 
-# 전체 맵 정보를 입력받ㄷ기
+# 전체 맵 정보를 입력 받기
 array = []
 for i in range(n):
     array.append(list(map(int, input().split(' '))))
 
+
 # 북, 동, 남, 서 방향 정의
 dx = [-1, 0, 1, 0]
-
 dy = [0, 1, 0, -1]
 
 # 왼쪽으로 회전
@@ -49,7 +45,6 @@ while True:
         count += 1
         turn_time = 0
         continue
-
     else:
         turn_time += 1
 
