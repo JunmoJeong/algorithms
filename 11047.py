3a4f9b469918997e44049253e958e985307cdad6
JunmoJeong <jjmstars7@gmail.com>
2022-02-23 07:39:27 +0900

    11047 solution

---
+++ 11047.py
@@ -0,0 +1,11 @@
+n, k = map(int, input().split())
+
+arr = []
+for i in range(n):
+    arr.append(int(input()))
+sum = 0
+for i in range(n-1, -1, -1):
+    sum = sum+(k//arr[i])
+    k = k % arr[i]
+
+print(sum)
