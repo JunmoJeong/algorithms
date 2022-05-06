600541ba6e9e066db41683d0952c21589dedc8c4
JunmoJeong <jjmstars7@gmail.com>
2022-05-06 21:49:26 +0900

    linux_cmd

--- linux_top_command.py
+++ linux_top_command.py
@@ -1,6 +1,3 @@
 import os
 
-top_cmd_result = os.popen('top | head -6').read()
-print(top_cmd_result)
-# print(type(top_cmd_result))
-# print(len(top_cmd_result
+top_cmd_result = os.popen('top | head -6 > test_top.txt')
