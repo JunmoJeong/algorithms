840a0b2b2f5883044651a8292ec9705853c9ab55
JunmoJeong <jjmstars7@gmail.com>
2022-09-18 19:39:44 +0900

    test

--- 2750.py
+++ 2750.py
@@ -1,6 +1,23 @@
-N = 10000
+'''N = 10000
 cnt = 1
 
 for i in range(N):
     print("연산 횟수:" + str(cnt))
+    cnt += 1
+'''
+
+
+N = 100000
+cnt = 1
+
+for i in range(N):
+    print("연산 횟수" + str(cnt))
+    cnt += 1
+
+for i in range(N):
+    print("연산 횟수" + str(cnt))
+    cnt += 1
+
+for i in range(N):
+    print("연산 횟수" + str(cnt))
     cnt += 1
