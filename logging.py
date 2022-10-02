13ff4844b76440ca6175f745b18ef80ccf4ead7f
JunmoJeong <jjmstars7@gmail.com>
2022-10-02 18:50:34 +0900

    logging test

--- logging.py
+++ logging.py
@@ -10,3 +10,11 @@ end_date = '20220930'
 date_list = pd.date_range(start=start_date, end=end_date, freq='D')
 # print(date_list)
 # print(len(date_list))
+
+# file_data['name'] = "computer"
+# file_data['languate'] = "kor"
+# file_data['words'] = {'ram': '램', "process": "프로세스",
+#                       'processor': '프로세서', 'CPU': "씨피유"}
+# file_data['number'] = 4
+
+# print(json.dumps(file_data, ensure_ascii=False, indent="\t"))
