c3aea9b869389980548b522a7fe65ccbaa2eb875
JunmoJeong <jjmstars7@gmail.com>
2022-10-02 18:02:19 +0900

    logging test

--- logging.py
+++ logging.py
@@ -1,8 +1,8 @@
 # from turtle import pd
 import pandas as pd
+import json
 start_date = '20210101'
 end_date = '20220930'
 date_list = pd.date_range(start=start_date, end=end_date, freq='D')
 # print(date_list)
 # print(len(date_list))
-s
