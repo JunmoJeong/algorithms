fa6f72c6b411cb0daaab9d850b651d7263bd64ab
JunmoJeong <jjmstars7@gmail.com>
2022-10-03 18:55:55 +0900

    log test

--- logging.py
+++ logging.py
@@ -2,12 +2,19 @@
 import pandas as pd
 import json
 from collections import OrderedDict
+import datetime
+
 
 file_data = OrderedDict()
 
 start_date = '20210101'
 end_date = '20220930'
 date_list = pd.date_range(start=start_date, end=end_date, freq='D')
+
+nowDate = datetime.datetime.now()
+# f = open(nowDate.strftime("%Y-%m-%d) + ".json","w")
+
+
 # print(date_list)
 # print(len(date_list))
 
@@ -17,4 +24,6 @@ date_list = pd.date_range(start=start_date, end=end_date, freq='D')
 #                       'processor': '프로세서', 'CPU': "씨피유"}
 # file_data['number'] = 4
 
-# print(json.dumps(file_data, ensure_ascii=False, indent="\t"))
+print(json.dumps(file_data, ensure_ascii=False, indent="\t"))
+with open(nowDate.strftime("%Y-%m-%d) + ".json", 'w', encoding='utf-8') as make_file:
+    json.dump(file_data, make_file, ensure_ascii=False, indent='\t')
