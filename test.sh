d50e6c1a91e77870db101dd3df8b5f92136e172a
JunmoJeong <jjmstars7@gmail.com>
2022-10-16 21:47:54 +0900

    scripts

--- test.sh
+++ test.sh
@@ -1,7 +1,12 @@
-#! /bin/bash
+#!/bin/bash
 
-echo "input something : "
-read input
-echo "your input : $input"
+#echo "input something : "
+#read input
+#echo "your input : $input"
 
+VAL1=4
+VAL2=4
 
+if [ $VAL1 -eq $VAL2 ] ; then
+echo "equal"
+fi
